4a31f6c21f8205fc5c2ffd3244cc1cd716f30d48
nikita <nikita.bosire@student.moringaschool.com>
2024-06-29 14:31:35 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = function(a, b) {
+    return a / b;
+};
+
+
+const square = a => a * a
+
+
+
+const add = (a, b) => {
+    return a + b;
+};
+
+
+console.log(divide(2000, 100)); 
+console.log(square (5) ); 
+console.log(add(3, 7)); 
